03130190c5eaba35a2405b255de02064f726abcb
ScarLion <lopezbravocarlosjesus@protonmail.com>
2026-02-18 11:08:46 -0500

    Quartz sync: Feb 18, 2026, 11:08 AM

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "carloslopez-ciber.github.io/ScarLion/",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
